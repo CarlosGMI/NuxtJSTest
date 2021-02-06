08e05eff7ec2c55ae30c40e2c4745c5b758b752a
carlosgmi <carlosmaldonado@correo.unimet.edu.ve>
2021-02-06 02:38:43 -0400

    :lipstick: :pencil2: :art: Add Instructions text. Modify header template formatting

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -2,30 +2,14 @@
   <header class="header-container py-1">
     <b-container>
       <b-row>
-        <b-col
-          cols="5"
-          sm="2"
-        >
-          <b-navbar-brand
-            class="m-0"
-            href="/"
-          >
-            <b-img
-              fluid
-              src="~/assets/images/logo.png"
-            />
+        <b-col cols="5" sm="2">
+          <b-navbar-brand class="m-0" href="/">
+            <b-img fluid src="~/assets/images/logo.png" />
           </b-navbar-brand>
         </b-col>
-        <b-col
-          cols="7"
-          sm="10"
-        >
+        <b-col cols="7" sm="10">
           <ul class="header-menu list-unstyled d-flex align-items-center m-0 h-100 pl-4">
-            <li
-              id="instructions-modal-opener"
-              v-b-modal.instructions-modal
-              class="text-uppercase font-main-gray"
-            >
+            <li id="instructions-modal-opener" v-b-modal.instructions-modal class="text-uppercase font-main-gray">
               Intrucciones
             </li>
           </ul>
--- components/app/AppInstructions.vue
+++ components/app/AppInstructions.vue
@@ -1,12 +1,15 @@
 <template>
-  <b-modal
-    id="instructions-modal"
-    :hide-footer="true"
-    title="Instrucciones"
-    :static="true"
-    :lazy="true"
-  >
-    <p>Instrucciones</p>
+  <b-modal id="instructions-modal" :hide-footer="true" title="Instrucciones" :static="true" :lazy="true">
+    <h4 class="mb-3">¡Bienvenido!</h4>
+    <p>En Imagenes Del Mundo nos caracterizamos por la excelencia de nuestras fotografías y, como somos un poco competitivos 😉, nos hemos enfocado en buscar cuál de todos nuestros vendedores es el que tiene mejores fotografías.</p>
+    <p>Para ello necesitamos de <span class="highlight">tu ayuda</span> y lo único que tienes que hacer es seguir estos sencillos pasos:</p>
+    <ol>
+      <li>Introduce en el campo de texto una palabra que desees buscar.</li>
+      <li>Haz click sobre el botón de "¿Qué hay?"</li>
+      <li>Cuando puedas ver los resultados, haz click sobre la imagen que más te guste y presiona el botón de "Me encanta".</li>
+    </ol>
+    <p><span class="highlight">¡Y listo!</span> Habrás dado 3 puntos al vendedor del cual seleccionaste la imagen, sin embargo es un proceso que puedes repetir hasta que consigas que algún vendedor alcance los 20 puntos. Cuando lo hagas, harás que el vendedor pueda crear una factura en Alegra que ¡hasta tú podrás ver!</p>
+    <p><i><span class="highlight">¡Tu ayuda es valiosa y juntos podemos hacer de este mundo un lugar más fotogénico!</span></i></p>
   </b-modal>
 </template>
 
@@ -16,6 +19,4 @@ export default {
 };
 </script>
 
-<style>
-
-</style>
+<style lang="scss"></style>
