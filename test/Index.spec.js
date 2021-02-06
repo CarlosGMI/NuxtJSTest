366af4cf993f4ea0dc882d91e0921b4775341abe
carlosgmi <carlosmaldonado@correo.unimet.edu.ve>
2021-02-06 19:01:11 -0400

    :white_check_mark: Add no working tests for Index (wasting too much time in TDD)

---
+++ test/Index.spec.js
@@ -0,0 +1,62 @@
+import { shallowMount, mount, createLocalVue } from '@vue/test-utils';
+import Index from '~/pages/index.vue';
+import Vuex from 'vuex';
+import sellersListMock from './__mocks__/sellersList.json';
+
+const localVue = createLocalVue();
+
+localVue.use(Vuex);
+
+describe('Index page', () => {
+  it('Calls "fetchSellersList" on mounted Index', () => {
+    const actions = {
+      'sellers/fetchSellersList': jest.fn(),
+    };
+    const getters = {
+      'sellers/getSellersList': jest.fn(),
+    };
+    const store = new Vuex.Store({
+      modules: {
+        sellers: {
+          actions,
+          getters,
+        },
+      },
+    });
+    const wrapper = mount(Index, {
+      store,
+      localVue,
+      stubs: {
+        RaceForm: true,
+      },
+    });
+
+    expect(actions['sellers/fetchSellersList']).toHaveBeenCalled();
+  });
+
+  it('"fetchSellersList" action commits an array', async () => {
+    jest.doMock('axios', () => ({
+      get: jest.fn(() => Promise.resolve({ sellers: sellersListMock })),
+    }));
+
+    const actions = {
+      'sellers/fetchSellersList': jest.fn(() =>
+        Promise.resolve({ sellers: sellersListMock })
+      ),
+    };
+    const getters = {
+      'sellers/getSellersList': jest.fn(),
+    };
+    const store = new Vuex.Store({
+      modules: {
+        sellers: {
+          actions,
+          getters,
+        },
+      },
+    });
+
+    await store.dispatch('sellers/fetchSellersList');
+    expect(store.getters['sellers/getSellersList']).toEqual(sellersListMock);
+  });
+});
