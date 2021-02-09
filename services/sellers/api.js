70db49dfc914cf37401f131a1a0fb3b1234269e4
carlosgmi <carlosmaldonado@correo.unimet.edu.ve>
2021-02-09 12:07:23 -0400

    Add log to see what is happening on Vercel

--- services/sellers/api.js
+++ services/sellers/api.js
@@ -2,6 +2,7 @@ import { alegraOptions } from '../request';
 
 export const Seller = $axios => ({
     getSellers() {
+        console.log(alegraOptions());
         return $axios('/sellers', {
             ...alegraOptions(),
             method: 'GET',
