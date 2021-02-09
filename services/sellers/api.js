f54b15960807a6f7046e9504f6355ce538017255
carlosgmi <carlosmaldonado@correo.unimet.edu.ve>
2021-02-09 17:25:31 -0400

    :wrench: :fire: Modify example env. Remove console.log

--- services/sellers/api.js
+++ services/sellers/api.js
@@ -2,7 +2,6 @@ import { alegraOptions } from '../request';
 
 export const Seller = $axios => ({
     getSellers() {
-        console.log(alegraOptions());
         return $axios('/sellers', {
             ...alegraOptions(),
             method: 'GET',
